671a2549d356e3fd2180632ebe6b44d2cf0fb754
RishiDhavala <84021105+RishiDhavala@users.noreply.github.com>
2023-04-19 08:42:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
-
 var mongoClient = require("mongodb").MongoClient;
-const MONGO_CONNECTION_URL = process.env.MONGO_CONNECTION_URL||"mongodb+srv://netflix:netflix@cluster0.yspy8dp.mongodb.net/test";
-console.log(MONGO_CONNECTION_URL);
+const MONGO_CONNECTION_URL = process.env.MONGO_CONNECTION_URL;
 mongoClient.connect(MONGO_CONNECTION_URL, function (err, client) {
   if (err) console.log(err);
   else console.log("Connected successfully to database");
-  const db = client.db("lostinspace");
+  let db = client.db("lostinspace");
   require("./server")(db);
 });
